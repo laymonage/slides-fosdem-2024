408b2dbb97b3e074e810c36f9dae8c31f2e509b7
Sage Abdullah <me@laymonage.com>
2024-01-28 12:15:37 +0000

    Add Shikiji config

---
+++ shikiji.ts
@@ -0,0 +1,11 @@
+/* ./setup/shikiji.ts */
+import { defineShikijiSetup } from '@slidev/types'
+
+export default defineShikijiSetup(() => {
+  return {
+    themes: {
+      dark: 'vitesse-dark',
+      light: 'vitesse-light',
+    },
+  }
+})
